ded3b6c3f00ba8049c93b27e72154bb5fdd4e91b
ako652 <akotabe0213@gmail.com>
2023-05-29 18:50:06 +0200

    final-work

--- src/components/countries/CountryList.tsx
+++ src/components/countries/CountryList.tsx
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+type Countrys = {
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      ara: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  cca2:string,
+  flags:{
+    png:string
+  }
+};
 
 export default function CountryList() {
-  return <div>CountryList</div>;
+  const [countries, setCountries] = useState<Countrys[]>([]);
+  const url = "https://restcountries.com/v3.1/all";
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((error) => error);
+  }, []);
+  return <div style={{display:'grid', gridTemplateColumns:'1fr 1fr'}}> <h1>CountryList</h1>{countries.map((country)=>{return(
+    <div key={country.cca2} >
+      <p>{country.name.common}</p>
+      <p>{country.name.official}</p>
+      <img src={country.flags.png}  style={{width:'200px'}} />
+    </div>
+  )})}</div>;
 }
--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -1,5 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type Products = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: {
+    image: string;
+  };
+};
 
 export default function ProductList() {
-  return <div>ProductList</div>;
+  const [products, setProducts] = useState<Products[]>([]);
+  const [filters, setFilters] = useState<Products[]>(products);
+  const [value, setValue] = useState("");
+  const url = "https://api.escuelajs.co/api/v1/products";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((error) => error);
+  }, []);
+  useEffect(() => {
+    const results = products.filter((product) =>
+      product.title.toLocaleLowerCase().includes(value)
+    );
+    setFilters(results);
+  }, [value, products]);
+  const onChangehandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+  return (
+    <div>
+      <h1>ProductList</h1>
+      <input
+        type="text"
+        placeholder="find your product"
+        onChange={onChangehandler}
+      />
+      <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr " }}>
+        {filters.map((product) => {
+          return (
+            <div key={product.id}>
+              <p>{product.title}</p>
+              <p>{product.price}</p>
+              <img src={product.category.image} style={{ width: "200px" }} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
